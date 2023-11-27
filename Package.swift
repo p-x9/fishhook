719f2f69e8ac1ece73b5e7d83b06554312be0ce1
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-11-27 13:04:44 +0900

    setup spm

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "fishhook",
+    products: [
+        .library(
+            name: "fishhook",
+            targets: ["fishhook"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "fishhook",
+            path: "./",
+            publicHeadersPath: "include/"
+        )
+    ]
+)
+
